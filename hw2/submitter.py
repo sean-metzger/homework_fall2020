71eb1618fad2ef2a6820db5e70b93da5a4286263
sean-metzger <sean.metzger@ucsf.edu>
2020-09-27 19:23:28 -0700

    updated

---
+++ hw2/submitter.py
@@ -0,0 +1,27 @@
+print('Sko Buffs')
+import subprocess
+import shlex
+import os 
+import sys
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+cs285_command = (' ').join(sys.argv[1:])
+print('command', cs285_command)
+basedir = '/userdata/smetzger/cs285/homework_fall2020/hw2/'
+times= now.strftime("%d_%m_%Y_%H_%M_%S")
+ 
+filename = basedir + 'run' + ('_').join(cs285_command.split(' ')[2:]) + '.txt'
+string = "submit_job -q mind-gpu"
+string += " -m 78 -g 1"
+string += " -o " + filename
+string += ' -n deeprl'
+string += ' -x python '
+string += basedir + (' ').join(cs285_command.split(' ')[1:])
+
+print(string)
+cmd = shlex.split(string)
+print(cmd)
+subprocess.run(cmd, stderr=subprocess.STDOUT)
